52409afd942153335ce76442fb1eb42eb5e78b96
yukyan <yukyanjpn@gmail.com>
2025-03-07 02:31:14 +0900

    go1.23.7ならできた。go1.24だとなんかpanic: syscall/js: call of Value.Int というエラーが出る

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ server:
 	python3 -m http.server 8000
 
 build:
-	GOOS=js GOARCH=wasm go build -o main.wasm
+	GOOS=js GOARCH=wasm GOTOOLCHAIN=go1.23.7 go build -o main.wasm main.go
--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"syscall/js"
 )
 
 func ProcessTextElements(_ js.Value, args []js.Value) any {
 	// JSON文字列を入力値として受け取る
 	jsonStr := args[0].String()
-	// JSONオブジェクトをそのまま返す
-	return jsonStr
+
+	// JSON文字列をデコード
+	var elements []map[string]interface{}
+	if err := json.Unmarshal([]byte(jsonStr), &elements); err != nil {
+		return "Error: " + err.Error()
+	}
+
+	// 各要素のtextフィールドを"hoge"に変更
+	for i := range elements {
+		elements[i]["text"] = "hoge"
+	}
+
+	// 変更したデータをJSON文字列に戻す
+	result, err := json.Marshal(elements)
+	if err != nil {
+		return "Error: " + err.Error()
+	}
+
+	return string(result)
 }
 
 func main() {
